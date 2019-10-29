1f82ef152e0273e94fb3dbbae5c2661678294885
noahfurniss <nfurniss@iu.edu>
2019-10-29 16:38:37 -0400

    Updates

---
+++ precache-manifest.b6e6fc821a94ff681213ca27f6e5a0ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b723514234822d70d734399c318ba4",
+    "url": "/spideyguy99/react-basics/index.html"
+  },
+  {
+    "revision": "42fbfd7de9addb684d04",
+    "url": "/spideyguy99/react-basics/static/css/main.a9a416fb.chunk.css"
+  },
+  {
+    "revision": "cd8c52c0f4a376241aee",
+    "url": "/spideyguy99/react-basics/static/js/2.e368de84.chunk.js"
+  },
+  {
+    "revision": "42fbfd7de9addb684d04",
+    "url": "/spideyguy99/react-basics/static/js/main.ae0fbaaa.chunk.js"
+  },
+  {
+    "revision": "3a867d07cd80a9a7cea9",
+    "url": "/spideyguy99/react-basics/static/js/runtime-main.44169ce4.js"
+  }
+]);
